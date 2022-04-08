c0fd5124b20ca5167ef5db4852481ce1ecd8d80c
Ali Irani <aliirani74@gmail.com>
2022-04-08 14:08:25 +0430

    Remove gis library because of 140MB additional space

--- restaurantsProject/restaurantManager/models/restaurant.py
+++ restaurantsProject/restaurantManager/models/restaurant.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.gis.db import models as gis_models
 
 from restaurantManager.models.base import BaseModel
 from restaurantManager.models.category import Category
@@ -12,7 +11,8 @@ class Restaurant(BaseModel):
     city = models.CharField(max_length=30, null=True)  # If we work only on Canada, We can add "Choices" to this field
     state = models.CharField(max_length=10, null=True)  # If we work only on Canada, We can add "Choices" to this field
     postal_code = models.CharField(max_length=16, null=True)
-    location = gis_models.PointField(null=True)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
     stars = models.FloatField(default=0)
     review_cnt = models.IntegerField(default=0)
     is_open = models.BooleanField(default=0)
