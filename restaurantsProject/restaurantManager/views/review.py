5a37c7f787bad520cb8f09cd0b1564b7136e0645
Ali Irani <aliirani74@gmail.com>
2022-04-09 16:49:48 +0430

    Add get and delete review view

--- restaurantsProject/restaurantManager/views/review.py
+++ restaurantsProject/restaurantManager/views/review.py
@@ -5,11 +5,33 @@ from restaurantManager.models import Review as ReviewModel
 from restaurantManager.serializers import Review as ReviewSerializer
 
 
-class ReviewList(mixins.CreateModelMixin, generics.ListAPIView):
-    # TODO: Permission
+# TODO: Permission
+# TODO: Swagger
+
+
+class ReviewDetail(generics.RetrieveDestroyAPIView):
     queryset = ReviewModel.objects.all()
-    # TODO: Swagger
+    serializer_class = ReviewSerializer
+    lookup_field = 'review_id'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.reviews.all()
+
+
+class ReviewList(mixins.CreateModelMixin, generics.ListAPIView):
+    queryset = ReviewModel.objects.all()
     serializer_class = ReviewSerializer
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.reviews.all()
