ca5b1f06811b29f228d79a8355b03458a1115628
Ali Irani <aliirani74@gmail.com>
2022-04-10 16:22:36 +0430

    Change user parent class and add auth token post save

--- restaurantsProject/restaurantManager/models/user.py
+++ restaurantsProject/restaurantManager/models/user.py
@@ -1,10 +1,13 @@
 from django.db import models, transaction
-from django.contrib.auth.models import User as DjangoUser
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 from restaurantManager.models.base import BaseModel
 
 
-class User(DjangoUser, BaseModel):
+class User(AbstractUser, BaseModel):
     user_id = models.CharField(max_length=32, null=False)
     name = models.CharField(max_length=30, null=False)
     useful = models.IntegerField(default=0)
@@ -16,3 +19,9 @@ class User(DjangoUser, BaseModel):
         self.average_stars = ((self.average_stars*self.stars_cnt)+star)/(self.stars_cnt+1)
         self.stars_cnt += 1
         self.save()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
