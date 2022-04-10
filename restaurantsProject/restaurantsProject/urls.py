a192899614c65437170630d74ae9438c231437a3
Ali Irani <aliirani74@gmail.com>
2022-04-10 18:13:57 +0430

    Add swagger urls

--- restaurantsProject/restaurantsProject/urls.py
+++ restaurantsProject/restaurantsProject/urls.py
@@ -17,9 +17,29 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Restaurant Manager API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^v1/api-token-auth/', views.obtain_auth_token),
     re_path(r'^v1/restaurant_manager/', include('restaurantManager.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
